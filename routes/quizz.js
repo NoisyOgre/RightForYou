a8855ab5b6361c0517c3826590ecb1ccafdcfb88
Ana C <apspfc@gmail.com>
2021-09-14 14:58:41 +0100

    asdfghjk

--- routes/quizz.js
+++ routes/quizz.js
@@ -1,64 +1,40 @@
 const router = require("express").Router();
 
-const preferences = {
-    experience : "",
-    goals: "",
-    time: "",
-    routine: "",
-    type: "",
-    budget: "",
-};
-
-router.get("/quiz1", (req,res)=>{
-res.render("quiz/quiz");
+router.get("/quiz1", (req, res) => {
+  res.render("quiz/quiz");
 });
 
-router.post("/quiz1", (req, res)=>{
+router.post("/quiz1", (req, res) => {
+  const { experience, goals, time, routine } = req.body;
+  preferences.experience = experience;
+  preferences.goals = goals;
+  preferences.time = time;
+  preferences.routine = routine;
+  /* console.log(experience,goals,time,routine); */
 
-const { experience, goals, time, routine} = req.body;
-preferences.experience= experience; 
-preferences.goals= goals;
-preferences.time =time;
-preferences.routine=routine;
-/* console.log(experience,goals,time,routine); */
-
-res.redirect("/quiz2");
+  res.redirect("/quiz2");
 });
 
-router.get("/quiz2", (req,res)=>{
-
-    res.render("quiz/skintype");
+router.get("/quiz2", (req, res) => {
+  res.render("quiz/skintype");
 });
 
-router.post("/quiz2", (req,res)=> {
+router.post("/quiz2", (req, res) => {
+  const { type } = req.body;
+  preferences.type = type;
 
-    const {type} = req.body;
-    preferences.type = type;
-
-    res.redirect("/quiz3")
-
+  res.redirect("/quiz3");
 });
 
-router.get("/quiz3", (req,res)=>{
-
-    res.render("quiz/budget");
+router.get("/quiz3", (req, res) => {
+  res.render("quiz/budget");
 });
 
-router.post("/quiz3", (req,res)=>{
-    
-    const {budget} = req.body;
-    preferences.budget= budget;
+router.post("/quiz3", (req, res) => {
+  const { budget } = req.body;
+  preferences.budget = budget;
 
-    res.render("user_interface/myroutine",preferences);
-
+  res.render("user_interface/myroutine", preferences);
 });
-
-
-
-
-
-
-
 
 module.exports = router;
-
