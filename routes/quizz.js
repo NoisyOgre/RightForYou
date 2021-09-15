a788932bc0c23366a07151589f6516343e687986
Ana C <apspfc@gmail.com>
2021-09-15 14:28:51 +0100

    "additions"

--- routes/quizz.js
+++ routes/quizz.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const test = require("../filter.js");
 
-const preferences = {
-}
+const preferences = {};
+const result = {};
 
 router.get("/quiz1", (req, res) => {
   res.render("quiz/quiz");
@@ -37,8 +37,63 @@ router.get("/quiz3", (req, res) => {
 router.post("/quiz3", (req, res) => {
   const { budget } = req.body;
   preferences.budget = budget;
-  test();
   res.render("user_interface/myroutine", preferences);
+});
+
+router.get("/my-personal-routine", async (req, res) => {
+  let filteredData = await test();
+  result.cleansers = filteredData.cleansers;
+  result.moisturizers = filteredData.moisturizers;
+  result.sunProtection = filteredData.sunProtection;
+
+  switch (preferences.type) {
+    case "normal":
+      result.type = filteredData.normalSkin;
+      break;
+    case "oily":
+      result.type = filteredData.oilySkin;
+      break;
+    case "dry":
+      result.type = filteredData.drySkin;
+      break;
+    case "combination":
+      result.type = filteredData.comboSkin;
+      break;
+    case "sensitive":
+      result.type = filteredData.senSkin;
+      break;
+  }
+
+  switch (preferences.goals) {
+    case "prevention":
+      result.goals = "prevention";
+      break;
+    case "keeping":
+      result.goals = "keeping";
+      break;
+    case "helping":
+      result.goals = "helping";
+      break;
+  }
+
+  switch (preferences.time) {
+    case "ten":
+      result.steps = 3;
+      break;
+    case "thirty":
+      result.steps = 6;
+      result.toners = filteredData.toners;
+      result.serums = filteredData.serums;
+      result.essence = filteredData.essence;
+      break;
+    case "thirtyPlus":
+      result.steps = 10;
+      result.mask = filteredData.mask;
+      break;
+  }
+
+  res.render("user_interface/myroutinefull", result);
+  
 });
 
 module.exports = router;
